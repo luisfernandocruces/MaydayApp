5488969d0f1afbe54963850573ee2cf0cc0cccdf
Jose Alejandro Galvis Nieto <jose.galvis@correo.icesi.edu.co>
2020-05-30 09:38:10 -0500

    user model created

---
+++ models/health_support.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Health_SupportSchema = new mongoose.Schema({
+    
+
+})
+
+module.exports = mongoose.model('Health_Support', Health_SupportSchema);
