c3828bb4351691c0005fe59527dc26fcd442f73e
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-30 11:55:28 -0500

    Work image

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -45,8 +45,7 @@
                                
                                 <card class="border-0" hover shadow body-classes="py-5">
                                     <icon name="ni ni-ambulance" type="primary" rounded class="mb-4">
-                                    </icon>                 
-                                    
+                                    </icon>                  
                                     <h6 class="text-primary text-uppercase">Personas</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
                                         that includes the most important components and features.</p>
@@ -65,8 +64,8 @@
                             </div>
                             <div class="col-lg-4">
                                 <card class="border-0" hover shadow body-classes="py-5">
-                                    <icon name="ni ni-single-copy-04" type="info" rounded class="mb-4">
-                                    </icon>
+                                    <img src="img/helps/work.png" alt="work" border="0" style="height: 150px;">
+
                                     <h6 class="text-info text-uppercase">Empresas</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap
                                         4 that includes the most important components and features.</p>
