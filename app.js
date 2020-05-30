99920fc4d0fe1e942aa78af682912c9bba19de18
luisfcv97 <luisfcv97@gmail.com>
2020-05-30 14:42:18 -0500

    fixing some problems

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -21,6 +22,7 @@ app.set("view engine", "jade");
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
