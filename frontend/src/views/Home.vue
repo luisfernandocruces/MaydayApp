49e40a4a7e50ec7f1b546c98217df0f958fecfae
luisfcv97 <luisfcv97@gmail.com>
2020-05-30 16:27:35 -0500

    spand incrret

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,36 +1,34 @@
 <template>
-    <div>
-        <div class="position-relative">
-            <!-- shape Hero -->
-            <section class="section-shaped my-0">
-                <div class="shape shape-style-1 bg-gradient-info shape-skew">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <div class="container shape-container d-flex">
-                    <div class="col px-0">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <h1 class="display-3  text-white">Una aplicación especial para la oferta de ayudas</span>
-                                </h1>
-                                <p class="lead  text-white">Esta pagina web te dara la capacidad de ofrecer y recibir ayudas a personas y empresas afectadas por el COVID-19, tanto para personas como para empresas se han definido diferentes ayudas.</p>
-                                <div class="btn-wrapper">
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- 1st Hero Variation -->
-      
+  <div>
+    <div class="position-relative">
+      <!-- shape Hero -->
+      <section class="section-shaped my-0">
+        <div class="shape shape-style-1 bg-gradient-info shape-skew">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div class="container shape-container d-flex">
+          <div class="col px-0">
+            <div class="row">
+              <div class="col-lg-6">
+                <h1 class="display-3 text-white">Una aplicación especial para la oferta de ayudas</h1>
+                <p
+                  class="lead text-white"
+                >Esta pagina web te dara la capacidad de ofrecer y recibir ayudas a personas y empresas afectadas por el COVID-19, tanto para personas como para empresas se han definido diferentes ayudas.</p>
+                <div class="btn-wrapper"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- 1st Hero Variation -->
     </div>
     <section class="section section-lg pt-lg-0 mt--200">
       <div class="container">
@@ -39,12 +37,7 @@
             <div class="row row-grid">
               <div class="col-lg-4">
                 <card class="border-0" hover shadow body-classes="py-5">
-                  <img
-                    src="img/helps/people.jpg"
-                    alt="people"
-                    border="0"
-                    style="height: 180px;"
-                  />
+                  <img src="img/helps/people.jpg" alt="people" border="0" style="height: 180px;" />
                   <h6 class="text-primary text-uppercase">Personas</h6>
                   <p class="description mt-3">
                     Aquí podrás encontrar información sobre diferentes tipos de
@@ -56,19 +49,12 @@
                     <badge type="primary" rounded>HUMANITARIA</badge>
                     <badge type="primary" rounded>PSICOLÓGICA</badge>
                   </div>
-                  <base-button tag="a" href="#" type="primary" class="mt-4">
-                    Entrar
-                  </base-button>
+                  <base-button tag="a" href="#" type="primary" class="mt-4">Entrar</base-button>
                 </card>
               </div>
               <div class="col-lg-4">
                 <card class="border-0" hover shadow body-classes="py-5">
-                  <img
-                    src="img/helps/work2.jpg"
-                    alt="work"
-                    border="0"
-                    style="height: 180px;"
-                  />
+                  <img src="img/helps/work2.jpg" alt="work" border="0" style="height: 180px;" />
 
                   <h6 class="text-info text-uppercase">Empresas</h6>
                   <p class="description mt-3">
@@ -81,9 +67,7 @@
                     <badge type="info" rounded>AYUDA DEL ESTADO</badge>
                     <badge type="info" rounded></badge>
                   </div>
-                  <base-button tag="a" href="#" type="info" class="mt-4">
-                    Entrar
-                  </base-button>
+                  <base-button tag="a" href="#" type="info" class="mt-4">Entrar</base-button>
                 </card>
               </div>
 
@@ -96,9 +80,7 @@
                     border="0"
                     style="height: 180px;"
                   />
-                  <h6 class="text-success text-uppercase" align>
-                    Otras Ayudas
-                  </h6>
+                  <h6 class="text-success text-uppercase" align>Otras Ayudas</h6>
                   <p class="description mt-3">
                     Aquí podrás encontrar información sobre cualquier otro tipo
                     ayuda.
@@ -114,9 +96,7 @@
                     href="#"
                     type="success"
                     class="mt-4 justify-content-center"
-                  >
-                    Entrar
-                  </base-button>
+                  >Entrar</base-button>
                 </card>
               </div>
             </div>
@@ -130,6 +110,6 @@
 <script>
 export default {
   name: "home",
-  components: {},
+  components: {}
 };
 </script>
