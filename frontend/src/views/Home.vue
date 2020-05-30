2eb520ff22c45cdd61d73d022a985d8df66abc39
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-30 11:51:56 -0500

    Home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -45,15 +45,16 @@
                                
                                 <card class="border-0" hover shadow body-classes="py-5">
                                     <icon name="ni ni-ambulance" type="primary" rounded class="mb-4">
-                                    </icon>
-                                     <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+                                    </icon>                 
+                                    
                                     <h6 class="text-primary text-uppercase">Personas</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
                                         that includes the most important components and features.</p>
                                     <div>
-                                        <badge type="primary" rounded>design</badge>
-                                        <badge type="primary" rounded>system</badge>
-                                        <badge type="primary" rounded>creative</badge>
+                                        <badge type="primary" rounded>MÉDICA</badge>
+                                        <badge type="primary" rounded>TRABAJO</badge>
+                                        <badge type="primary" rounded>HUMANITARIA</badge>
+                                        <badge type="primary" rounded>PSICOLÓGICA</badge>
                                     </div>
                                     <base-button tag="a" href="#" type="primary" class="mt-4">
                                         Entrar
@@ -64,38 +65,42 @@
                             </div>
                             <div class="col-lg-4">
                                 <card class="border-0" hover shadow body-classes="py-5">
-                                    <icon name="ni ni-single-copy-04" type="success" rounded class="mb-4">
+                                    <icon name="ni ni-single-copy-04" type="info" rounded class="mb-4">
                                     </icon>
-                                    <h6 class="text-success text-uppercase">Empresas</h6>
+                                    <h6 class="text-info text-uppercase">Empresas</h6>
                                     <p class="description mt-3">Argon is a great free UI package based on Bootstrap
                                         4 that includes the most important components and features.</p>
                                     <div>
-                                        <badge type="success" rounded>business</badge>
-                                        <badge type="success" rounded>vision</badge>
-                                        <badge type="success" rounded>success</badge>
+                                        <badge type="info" rounded>BIOSEGURIDAD</badge>
+                                        <badge type="info" rounded>EMPRENDIMIENTO</badge>
+                                        <badge type="info" rounded>AYUDA DEL ESTADO</badge>
+                                        <badge type="info" rounded></badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="info" class="mt-4">
+                                        Entrar
+                                    </base-button>
+                                </card>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-single-copy-04" type="success" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-success text-uppercase" align>Otras Ayudas</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="success" rounded>LEGALES</badge>
+                                        <badge type="success" rounded>CONTÁCTANOS</badge>
+                                        <badge type="success" rounded>PERSONAS ESPECIALES</badge>
+                                        <badge type="success" rounded></badge>
                                     </div>
                                     <base-button tag="a" href="#" type="success" class="mt-4">
                                         Entrar
                                     </base-button>
                                 </card>
                             </div>
-                            <div class="col-lg-4">
-                                <card class="border-0" hover shadow body-classes="py-5">
-                                    <icon name="ni ni-world-2" type="warning" rounded class="mb-4">
-                                    </icon>
-                                    <h6 class="text-warning text-uppercase">Más Información</h6>
-                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
-                                        4 that includes the most important components and features.</p>
-                                    <div>
-                                        <badge type="warning" rounded>marketing</badge>
-                                        <badge type="warning" rounded>product</badge>
-                                        <badge type="warning" rounded>launch</badge>
-                                    </div>
-                                    <base-button tag="a" href="#" type="warning" class="mt-4">
-                                        Entrar
-                                    </base-button>
-                                </card>
-                            </div>
+
                         </div>
                     </div>
                 </div>
