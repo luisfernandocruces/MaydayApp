5b16fb4c0e974304dd436638c09a4e31bee7c89f
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-30 13:45:00 -0500

    Home images

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -13,66 +13,6 @@
           <span></span>
           <span></span>
         </div>
-<<<<<<< HEAD
-        <div class="container shape-container d-flex">
-          <div class="col px-0">
-            <div class="row">
-              <div class="col-lg-6">
-                <h1 class="display-3 text-white">
-                  A beautiful Design System
-                  <span>completed with examples</span>
-                </h1>
-                <p class="lead text-white">
-                  The design system comes with four pre-built pages to help
-                  you get started faster. You can change the text and images and you're good to
-                  go.
-                </p>
-                <div class="btn-wrapper"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-    <section class="section section-lg pt-lg-0 mt--200">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-12">
-            <div class="row row-grid">
-              <div class="col-lg-4">
-                <card class="border-0" hover shadow body-classes="py-5">
-                  <img src="img/helps/people.jpg" alt="people" border="0" style="height: 180px;" />
-                  <h6 class="text-primary text-uppercase">Personas</h6>
-                  <p
-                    class="description mt-3"
-                  >Aquí podrás encontrar información sobre diferentes tipos de ayudas para personas.</p>
-                  <div>
-                    <badge type="primary" rounded>MÉDICA</badge>
-                    <badge type="primary" rounded>TRABAJO</badge>
-                    <badge type="primary" rounded>HUMANITARIA</badge>
-                    <badge type="primary" rounded>PSICOLÓGICA</badge>
-                  </div>
-                  <base-button tag="a" href="#" type="primary" class="mt-4">Entrar</base-button>
-                </card>
-              </div>
-              <div class="col-lg-4">
-                <card class="border-0" hover shadow body-classes="py-5">
-                  <img src="img/helps/work2.jpg" alt="work" border="0" style="height: 180px;" />
-
-                  <h6 class="text-info text-uppercase">Empresas</h6>
-                  <p
-                    class="description mt-3"
-                  >Aquí podrás encontrar información sobre diferentes tipos de ayudas para empresas.</p>
-                  <div>
-                    <badge type="info" rounded>BIOSEGURIDAD</badge>
-                    <badge type="info" rounded>EMPRENDIMIENTO</badge>
-                    <badge type="info" rounded>AYUDA DEL ESTADO</badge>
-                    <badge type="info" rounded></badge>
-                  </div>
-                  <base-button tag="a" href="#" type="info" class="mt-4">Entrar</base-button>
-                </card>
-              </div>
-=======
         <section class="section section-lg pt-lg-0 mt--200">
             <div class="container">
                 <div class="row justify-content-center" >
@@ -132,7 +72,6 @@
                                     </base-button>
                                 </card>
                             </div>
->>>>>>> 5d6c1969ac5146e3247ae93098f90fca350dfa20
 
               <div class="col-lg-4">
                 <card class="border-0" hover shadow body-classes="py-5">
