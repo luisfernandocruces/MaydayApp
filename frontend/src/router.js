552ad2ad92a2f75a89343ced53a491c8ed3c7108
Daniela Llano <daniela.llano1@correo.icesi.edu.co>
2020-05-30 10:13:35 -0500

    prueba del front

---
+++ frontend/src/router.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Router from "vue-router";
+import AppHeader from "./views/AppHeader";
+
+
+import Home from "./views/Home.vue";
+
+
+
+
+Vue.use(Router);
+
+export default new Router({
+  linkExactActiveClass: "active",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      components: {
+        header: AppHeader,
+        default: Home
+      
+      }
+    
+    }
+  ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+});
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">A beautiful Design System
+                                    <span>completed with examples</span>
+                                </h1>
+                                <p class="lead  text-white">The design system comes with four pre-built pages to help
+                                    you get started faster. You can change the text and images and you're good to
+                                    go.</p>
+                                <div class="btn-wrapper">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+        </div>
+        <section class="section section-lg pt-lg-0 mt--200">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="row row-grid">
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-ambulance" type="primary" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-primary text-uppercase">Ayuda Medica</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
+                                        that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="primary" rounded>design</badge>
+                                        <badge type="primary" rounded>system</badge>
+                                        <badge type="primary" rounded>creative</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="primary" class="mt-4">
+                                        Entrar
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-single-copy-04" type="success" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-success text-uppercase">Ayuda Laboral</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="success" rounded>business</badge>
+                                        <badge type="success" rounded>vision</badge>
+                                        <badge type="success" rounded>success</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="success" class="mt-4">
+                                        Entrar
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-world-2" type="warning" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-warning text-uppercase">Ayuda Humanitaria</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="warning" rounded>marketing</badge>
+                                        <badge type="warning" rounded>product</badge>
+                                        <badge type="warning" rounded>launch</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="warning" class="mt-4">
+                                        Entrar
+                                    </base-button>
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+      
+        
+        
+      
+        
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
