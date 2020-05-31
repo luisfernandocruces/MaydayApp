5a2dd6f20c8d4cb5c9b02b99d17cda11664adc3e
Jose Alejandro Galvis Nieto <jose.galvis@correo.icesi.edu.co>
2020-05-31 13:43:08 -0500

    details

--- frontend/src/views/ChatList.vue
+++ frontend/src/views/ChatList.vue
@@ -10,22 +10,30 @@
         <span></span>
         <span></span>
       </div>
-
     </section>
     <section class="section section-skew" style="margin-top:-100px">
       <div class="container">
         <div class="row">
           <div class="col">
-
-            <card class="border-0" hover shadow body-classes="py-5" style="margin_bottom:20px" 
-                v-for="user in usersWithChat" :key="user.email">
-                <h6 class="text-info text-uppercase">{{user.first_name}} {{user.last_name}}</h6>
-                <p class="description mt-3">{{user.email}}</p>
-                <base-button type="info" class="mt-4" @click="goToPrivateChat(user.email)">
-                    Ver Chat
-                    <!-- <router-link :to="{ name: 'privateChat',
-                                        params: { other_user: user.email }}" style="color:white">Ver Chat</router-link> -->
-                </base-button>
+            <h1 v-if="userRole == 'health professional'">PACIENTES:</h1>
+            <h1 v-if="userRole == 'normal person'">PROFESIONALES DE LA SALUD:</h1>
+            <br />
+            <card
+              class="border-0"
+              hover
+              shadow
+              body-classes="py-5"
+              style="margin_bottom:20px"
+              v-for="(user, index) in usersWithChat"
+              :key="index"
+            >
+              <h6 class="text-info text-uppercase">{{otherUserNames[index]}}</h6>
+              <p class="description mt-3">{{user.email}}</p>
+              <base-button type="info" class="mt-4" @click="goToPrivateChat(user.email)">
+                Ver Chat
+                <!-- <router-link :to="{ name: 'privateChat',
+                params: { other_user: user.email }}" style="color:white">Ver Chat</router-link>-->
+              </base-button>
             </card>
           </div>
         </div>
@@ -38,25 +46,31 @@ import axios from "../plugins/axios";
 export default {
   data() {
     return {
+      userRole: this.$store.state.user.rol,
       usersWithChat: [],
+      otherUserNames: []
     };
   },
 
-  created(){
-    console.log('Componente Creado!');
+  created() {
     this.loadChats();
   },
 
   methods: {
-      goToPrivateChat(other_email) {
-        this.$store.commit('updateToUserEmail', other_email);
-        this.$router.push('/privateChat'); 
-      },
-      loadChats(){
-        console.log('Holaaaaa');
-        axios.get('/conversation/allChats/' + this.$store.state.user._id).then(response => {
-            this.usersWithChat = response.data});
-      },
+    goToPrivateChat(other_email) {
+      this.$store.commit("updateToUserEmail", other_email);
+      this.$router.push("/privateChat");
+    },
+    loadChats() {
+      axios
+        .get("/conversation/allChats/" + this.$store.state.user._id)
+        .then(response => {
+          this.usersWithChat = response.data;
+          this.usersWithChat.forEach(element => {
+            axios.get("/users/" + element.email).then(response => {});
+          });
+        });
+    }
   }
 };
 </script>
