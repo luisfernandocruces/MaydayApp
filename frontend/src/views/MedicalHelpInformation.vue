ccb0c36dbb214e87588a5f72e223c5b8c849f29e
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-31 08:33:16 -0500

    Change structure

--- frontend/src/views/MedicalHelpInformation.vue
+++ frontend/src/views/MedicalHelpInformation.vue
@@ -118,7 +118,7 @@
                 <card class="border-0" hover shadow body-classes="py-5">
                   <img src="img/myths/medicines.png" class="img-fluid floating" style="width:100px" />
                   <h6
-                    class="text-primary text-uppercase"
+                    class="text-warning text-uppercase"
                     style="margin-top: 20px"
                   >DEMOSTRADO: Actualmente no existe ningún medicamento autorizado para tratar o prevenir la COVID-19</h6>
                   <p
@@ -131,7 +131,7 @@
                 <card class="border-0" hover shadow body-classes="py-5">
                   <img src="img/myths/beer.png" class="img-fluid floating" style="width:100px" />
                   <h6
-                    class="text-primary text-uppercase"
+                    class="text-warning text-uppercase"
                     style="margin-top: 20px"
                   >DEMOSTRADO: Beber alcohol no lo protegerá de la COVID19 y podría ser peligroso</h6>
                   <p
@@ -144,7 +144,7 @@
                 <card class="border-0" hover shadow body-classes="py-5">
                   <img src="img/myths/uv.png" class="img-fluid floating" style="width:100px" />
                   <h6
-                    class="text-success text-uppercase"
+                    class="text-warning text-uppercase"
                     style="margin-top: 20px"
                   >DEMOSTRADO: No deben utilizarse lámparas de luz ultravioleta (UV) para desinfectar las manos u otras zonas de la piel.</h6>
                   <p
@@ -172,55 +172,8 @@
       </div>
     </section>
 
-    <section class="section section-lg">
-      <div class="container">
-        <div class="row row-grid align-items-center">
-          <div class="col-md-6 order-md-2">
-            <img src="img/theme/promo-1.png" class="img-fluid floating" />
-          </div>
-          <div class="col-md-6 order-md-1">
-            <div class="pr-md-5">
-              <icon
-                name="ni ni-settings-gear-65"
-                class="mb-5"
-                size="lg"
-                type="success"
-                shadow
-                rounded
-              ></icon>
-              <h3>Awesome features</h3>
-              <p>
-                The kit comes with three pre-built pages to help you get started faster. You can change
-                the text and images and you're good to go.
-              </p>
-              <ul class="list-unstyled mt-5">
-                <li class="py-2">
-                  <div class="d-flex align-items-center">
-                    <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
-                    <h6 class="mb-0">Carefully crafted components</h6>
-                  </div>
-                </li>
-                <li class="py-2">
-                  <div class="d-flex align-items-center">
-                    <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
-                    <h6 class="mb-0">Amazing page examples</h6>
-                  </div>
-                </li>
-                <li class="py-2">
-                  <div class="d-flex align-items-center">
-                    <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
-                    <h6 class="mb-0">Super friendly support team</h6>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
     <section class="section section section-shaped my-0 overflow-hidden">
-      <div class="shape shape-style-1 bg-gradient-warning shape-skew">
+      <div class="shape shape-style-1 bg-gradient-success shape-skew">
         <span></span>
         <span></span>
         <span></span>
@@ -285,6 +238,53 @@
                 </div>
               </div>
             </card>
+          </div>
+        </div>
+      </div>
+    </section>
+
+     <section class="section section-lg">
+      <div class="container">
+        <div class="row row-grid align-items-center">
+          <div class="col-md-6 order-md-2">
+            <img src="img/theme/promo-1.png" class="img-fluid floating" />
+          </div>
+          <div class="col-md-6 order-md-1">
+            <div class="pr-md-5">
+              <icon
+                name="ni ni-settings-gear-65"
+                class="mb-5"
+                size="lg"
+                type="success"
+                shadow
+                rounded
+              ></icon>
+              <h3>Awesome features</h3>
+              <p>
+                The kit comes with three pre-built pages to help you get started faster. You can change
+                the text and images and you're good to go.
+              </p>
+              <ul class="list-unstyled mt-5">
+                <li class="py-2">
+                  <div class="d-flex align-items-center">
+                    <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
+                    <h6 class="mb-0">Carefully crafted components</h6>
+                  </div>
+                </li>
+                <li class="py-2">
+                  <div class="d-flex align-items-center">
+                    <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
+                    <h6 class="mb-0">Amazing page examples</h6>
+                  </div>
+                </li>
+                <li class="py-2">
+                  <div class="d-flex align-items-center">
+                    <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
+                    <h6 class="mb-0">Super friendly support team</h6>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
