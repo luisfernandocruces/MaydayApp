499c46b1bef5d26122810a698d973b7d1a4ceb8b
danielgalvis98 <dangaltor@gmail.com>
2020-05-30 20:40:57 -0500

    Added Stack overflow source

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -222,16 +222,14 @@ components: {
       }
     },
 
+    //Taken From: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
     validEmail: function (email) {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
     },
 
     checkForm: function (e) {
-      if (this.name && this.age) {
-        return true;
-      }
-
+      
       this.errors = [];
 
       if (!this.user.first_name) {
