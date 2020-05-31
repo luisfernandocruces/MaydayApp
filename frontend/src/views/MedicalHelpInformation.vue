faf2577417c11dfad24bc94d37caa26bd1323082
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-05-31 08:29:33 -0500

    Myths

--- frontend/src/views/MedicalHelpInformation.vue
+++ frontend/src/views/MedicalHelpInformation.vue
@@ -55,13 +55,12 @@
       </div>
     </section>
 
-
-        <section class="section bg-secondary">
+    <section class="section bg-secondary">
       <div class="container">
         <div class="row row-grid align-items-center">
-          <div class="col-md-6">
+          <div class="col-md-6 text-center">
             <div class="card bg-default shadow border-0">
-              <img v-lazy="'img/communication.png'" class="card-img-top" style="margin-top:20px"/>
+              <img v-lazy="'img/communication.png'" class="card-img-top" style="margin-top:20px" />
               <blockquote class="card-blockquote">
                 <svg
                   preserveAspectRatio="none"
@@ -73,44 +72,105 @@
                   <polygon points="0,42 583,95 683,0 0,95" opacity=".2" class="fill-default" />
                 </svg>
                 <h4 class="display-3 font-weight-bold text-white">Canales de información confiables</h4>
-                <p class="lead text-italic text-white">
-                  The Arctic Ocean freezes every winter and much of
-                  the sea-ice then thaws every summer, and that process will continue whatever
-                  happens.
-                </p>
               </blockquote>
             </div>
           </div>
           <div class="col-md-6">
             <div class="pl-md-5">
               <icon name="ni ni" class="mb-5" size="lg" type="warning" shadow rounded></icon>
-              <icon name="ni ni" class="mb-5" size="lg" type="warning" shadow rounded style="margin-left:5px"></icon>
-              <icon name="ni ni" class="mb-5" size="lg" type="warning" shadow rounded style="margin-left:5px"></icon>
+              <icon
+                name="ni ni"
+                class="mb-5"
+                size="lg"
+                type="warning"
+                shadow
+                rounded
+                style="margin-left:5px"
+              ></icon>
+              <icon
+                name="ni ni"
+                class="mb-5"
+                size="lg"
+                type="warning"
+                shadow
+                rounded
+                style="margin-left:5px"
+              ></icon>
 
-              <h3>Our customers</h3>
-              <p class="lead">
-                Don't let your uses guess by attaching tooltips and popoves to any element.
-                Just make sure you enable them first via JavaScript.
-              </p>
-              <p>
-                The kit comes with three pre-built pages to help you get started faster. You can change
-                the text and images and you're good to go.
-              </p>
-              <p>
-                The kit comes with three pre-built pages to help you get started faster. You can change
-                the text and images and you're good to go.
-              </p>
-              <a href="#" class="font-weight-bold text-warning mt-5">
-                A beautiful UI Kit for impactful
-                websites
-              </a>
+              <h3>Organización Mundial de la Salud</h3>
+              <p>La función principal de la OMS es dirigir la salud internacional dentro del sistema de las Naciones Unidas y liderar a los socios en las respuestas sanitarias mundiales.</p>
+              <a
+                href="https://www.who.int/es"
+                class="font-weight-bold text-warning mt-5"
+              >Website Oficial</a>
             </div>
           </div>
         </div>
       </div>
     </section>
 
-
+    <section class="section section-lg pt-lg-0" style="margin-top:50px">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-12">
+            <div class="row row-grid">
+              <div class="col-lg-3 text-center">
+                <card class="border-0" hover shadow body-classes="py-5">
+                  <img src="img/myths/medicines.png" class="img-fluid floating" style="width:100px" />
+                  <h6
+                    class="text-primary text-uppercase"
+                    style="margin-top: 20px"
+                  >DEMOSTRADO: Actualmente no existe ningún medicamento autorizado para tratar o prevenir la COVID-19</h6>
+                  <p
+                    class="description mt-3"
+                  >Aunque están en marcha varios ensayos de medicamentos, hasta el momento no se ha demostrado que la hidroxicloroquina ni ningún otro fármaco puedan curar o prevenir la COVID-19.</p>
+                  <div></div>
+                </card>
+              </div>
+              <div class="col-lg-3 text-center">
+                <card class="border-0" hover shadow body-classes="py-5">
+                  <img src="img/myths/beer.png" class="img-fluid floating" style="width:100px" />
+                  <h6
+                    class="text-primary text-uppercase"
+                    style="margin-top: 20px"
+                  >DEMOSTRADO: Beber alcohol no lo protegerá de la COVID19 y podría ser peligroso</h6>
+                  <p
+                    class="description mt-3"
+                  >El consumo frecuente o excesivo de alcohol puede aumentar el riesgo de sufrir problemas de salud.</p>
+                  <div></div>
+                </card>
+              </div>
+              <div class="col-lg-3 text-center">
+                <card class="border-0" hover shadow body-classes="py-5">
+                  <img src="img/myths/uv.png" class="img-fluid floating" style="width:100px" />
+                  <h6
+                    class="text-success text-uppercase"
+                    style="margin-top: 20px"
+                  >DEMOSTRADO: No deben utilizarse lámparas de luz ultravioleta (UV) para desinfectar las manos u otras zonas de la piel.</h6>
+                  <p
+                    class="description mt-3"
+                  >La radiación UV puede irritar la piel y dañar los ojos. El modo más eficaz de eliminar los virus es limpiarse las manos con un gel desinfectante hidroalcohólico o lavárselas con agua y jabón.</p>
+                  <div></div>
+                </card>
+              </div>
+              <div class="col-lg-3 text-center">
+                <card class="border-0" hover shadow body-classes="py-5">
+                 <img src="img/myths/temperature.png" class="img-fluid floating" style="width:100px" />
+                  <h6
+                    class="text-warning text-uppercase"
+                    style="margin-top: 20px"
+                  >REALIDAD: los termómetros sin contacto NO detectan la COVID-19</h6>
+                  <p
+                    class="description mt-3"
+                  >Los termómetros sin contacto resultan eficaces para detectar a personas con fiebre (es decir, con una temperatura corporal superior a la normal). Sin embargo, no permiten detectar a personas infectadas por el virus de la COVID-19.</p>
+                  <div></div>
+                </card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <section class="section section-lg">
       <div class="container">
@@ -158,36 +218,6 @@
         </div>
       </div>
     </section>
-
-    <section class>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col">
-            <div class="row row-grid">
-              <div class="col">
-                <card class="border-0 text-center" hover shadow body-classes="py-5">
-                  <h6 class="text-primary text-uppercase">VIDEOS INFORMATIVOS</h6>
-                  <br />
-                  <div class="col">
-                    <iframe
-                      width="600"
-                      height="338"
-                      src="https://www.youtube.com/embed/NMmAj1EKdVo"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; 
-                  encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen
-                    ></iframe>
-                  </div>
-                </card>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-
 
     <section class="section section section-shaped my-0 overflow-hidden">
       <div class="shape shape-style-1 bg-gradient-warning shape-skew">
