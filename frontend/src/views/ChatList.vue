3a63d0f466e3e976acfc909fff58930c7fe4ecbb
danielgalvis98 <dangaltor@gmail.com>
2020-05-31 06:59:03 -0500

    Little comments

--- frontend/src/views/ChatList.vue
+++ frontend/src/views/ChatList.vue
@@ -37,9 +37,9 @@
 export default {
   data() {
     return {
-      usersWithChat: [{first_name: 'Daniel', last_name: 'Galvis', email: "a@a.com"},
-        {first_name: 'Sara', last_name: 'Ortiz', email: "a@b.com"},
-        {first_name: 'Daniela', last_name: 'Llano', email: "a@c.com"},
+      usersWithChat: [{first_name: 'Daniel', last_name: 'Galvis', email: "tomas@gmail.com"},
+        {first_name: 'Sara', last_name: 'Ortiz', email: "daniel@gmail.com"},
+        {first_name: 'Daniela', last_name: 'Llano', email: "jose@gmail.com"},
         {first_name: 'Oswaldo', last_name: 'Ortiz', email: "a@d.com"},
         {first_name: 'Jose', last_name: 'Galvis', email: "a@e.com"},
         {first_name: 'Luis Fernando', last_name: 'Cruces', email: "a@f.com"},
